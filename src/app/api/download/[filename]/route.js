6df3449d1fce2740a86602391a9b0a230f219484
Jose Luis Bedoya <wildchamo@gmail.com>
2024-03-30 13:20:34 -0500

    ready proxy server

--- src/app/api/download/[filename]/route.js
+++ src/app/api/download/[filename]/route.js
@@ -1,14 +1,13 @@
 // export an async GET function. This is a convention in NextJS
-export async function GET(req, { params }) {
+export async function GET(request, { params }) {
   // filename for the file that the user is trying to download
   const filename = params.filename;
 
-  // external file URL
-  const DUMMY_URL =
-    "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+  // Get the 'url' search parameter
+  const urlParam = new URL(request.url).searchParams.get("url");
 
   // use fetch to get a response
-  const response = await fetch(DUMMY_URL);
+  const response = await fetch(urlParam);
 
   // return a new response but use 'content-disposition' to suggest saving the file to the user's computer
   return new Response(response.body, {
--- src/app/home/seguros/[id]/page.js
+++ src/app/home/seguros/[id]/page.js
@@ -1,7 +1,6 @@
 "use client";
 import useUserStore from "../../../stores/userStore";
 import { useState, useEffect } from "react";
-import { companiasSeguros } from "../../../utils/insuranceCompaniesList";
 
 import { useRouter } from "next/navigation";
 
@@ -33,13 +32,16 @@ const SeguroIndividual = ({ params }) => {
 
   const downloadPdf = async () => {
     try {
-      const response = await axios.get("/api/download/dummy.pdf", {
-        responseType: "blob",
-      });
+      const response = await axios.get(
+        `/api/download/poliza.pdf?url=${seguro.documentos[0]}`,
+        {
+          responseType: "blob",
+        }
+      );
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "dummy.pdf");
+      link.setAttribute("download", "poliza.pdf");
       document.body.appendChild(link);
       link.click();
     } catch (error) {
@@ -145,16 +147,10 @@ const SeguroIndividual = ({ params }) => {
 
         {seguro?.documentos?.[0] ? (
           <section className="grid grid-cols-2">
-            <p className="text-left" onClick={downloadPdf}>
-              Poliza
-            </p>
-            <a
-              href={seguro?.documentos?.[0]}
-              download={seguro?.documentos?.[0]}
-              className="text-right underline"
-            >
+            <p className="text-left">Poliza</p>
+            <p onClick={downloadPdf} className="text-right underline">
               Visualizar
-            </a>
+            </p>
           </section>
         ) : (
           <p>No hay documentos adjuntos</p>
