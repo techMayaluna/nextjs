349aa09d46c0dfad9ae37dd819dafd008704dc52
Jose Luis Bedoya <wildchamo@gmail.com>
2024-05-01 14:42:34 -0500

    better backend code using a varaible

--- src/app/api/report/route.js
+++ src/app/api/report/route.js
@@ -13,12 +13,14 @@ export async function POST(request) {
   const report = new Report(requestBody);
   await report.save();
 
+  const urlReporte = `https://mayaluna-admin-three.vercel.app/home/reporte/${report._id}`;
+  
   try {
     const data = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["emaya@mayalunaseguros.com"],
       subject: "Envio de reporte de siniestro",
-     html: `El usuario ${report.nombre} ha enviado un reporte de siniestro. La placa del vehiculo es ${report.placaDelVehiculo}. Puede verlo en el siguiente enlace: <a href="https://mayaluna-admin-three.vercel.app/home/reporte/${report._id}">https://mayaluna-admin-three.vercel.app/home/reporte/${report._id}</a>`
+      html: `El usuario ${report.nombre} ha enviado un reporte de siniestro. La placa del vehiculo es ${report.placaDelVehiculo}. Puede verlo en el siguiente enlace: <a href="${urlReporte}">${urlReporte}</a>`
     });
   } catch (error) {
     console.log(error);
