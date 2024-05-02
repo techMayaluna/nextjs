2c717ed02434fe31d47267a78d6e0312347925a9
Jose Luis Bedoya <wildchamo@gmail.com>
2024-05-01 21:14:18 -0500

    update mail

--- src/app/api/report/route.js
+++ src/app/api/report/route.js
@@ -8,8 +8,6 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require("twilio")(acountSid, authToken);
 
-const resend = new Resend(process.env.RESEND_URI);
-
 export async function POST(request) {
   await connectDB();
   console.log("Request body", request.body);
@@ -19,17 +17,6 @@ export async function POST(request) {
   await report.save();
 
   const urlReporte = `https://mayaluna-admin-three.vercel.app/home/reporte/${report._id}`;
-
-  try {
-    await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["emaya@mayalunaseguros.com"],
-      subject: "Envio de reporte de siniestro",
-      html: `El usuario ${report.nombre} ha enviado un reporte de siniestro. La placa del vehiculo es ${report.placaDelVehiculo}. Puede verlo en el siguiente enlace: <a href="${urlReporte}">${urlReporte}</a>`
-    });
-  } catch (error) {
-    console.log(error);
-  }
 
   const numbers = [3142758675, 3244924827]; 
 
