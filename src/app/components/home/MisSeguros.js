146b7c967695621e1c93f5d9ddc8733bb7340bc3
Jose Luis <wildchamo@gmail.com>
2024-12-10 11:53:19 -0500

    update dependency array

--- src/app/components/home/MisSeguros.js
+++ src/app/components/home/MisSeguros.js
@@ -36,36 +36,36 @@ function MisSeguros() {
         });
       }
     });
-  }, [rol]);
+  }, [seguros, rol, getSeguros, _id]);
 
   return (
     <>
-        <>
-          <section className="pt-6">
-            <h2 className="mb-1">MIS SEGUROS</h2>
+      <>
+        <section className="pt-6">
+          <h2 className="mb-1">MIS SEGUROS</h2>
 
-            <div className="flex justify-between bg-primary p-6 rounded-2xl text-sm">
-              {seguros.length > 0 ? (
-                <>
-                  {seguros.slice(0, 2).map((seguro) => (
-                    <Link href={`/home/seguros/${seguro._id}`} key={seguro._id}>
-                      <InsuranceButton text={seguro.tipoPoliza} />
-                    </Link>
-                  ))}
-
-                  <Link href="/home/seguros">
-                    <ActionSection src="todos" text="Ver todos" />
+          <div className="flex justify-between bg-primary p-6 rounded-2xl text-sm">
+            {seguros.length > 0 ? (
+              <>
+                {seguros.slice(0, 2).map((seguro) => (
+                  <Link href={`/home/seguros/${seguro._id}`} key={seguro._id}>
+                    <InsuranceButton text={seguro.tipoPoliza} />
                   </Link>
-                </>
-              ) : (
-                <p className="text-md">
-                  Aún no tienes seguros! Comunicate con nuestro equipo para
-                  adquirir uno.
-                </p>
-              )}
-            </div>
-          </section>
-        </>
+                ))}
+
+                <Link href="/home/seguros">
+                  <ActionSection src="todos" text="Ver todos" />
+                </Link>
+              </>
+            ) : (
+              <p className="text-md">
+                Aún no tienes seguros! Comunicate con nuestro equipo para
+                adquirir uno.
+              </p>
+            )}
+          </div>
+        </section>
+      </>
 
       {rol !== "Colectivo" ? (
         <MisReportes
